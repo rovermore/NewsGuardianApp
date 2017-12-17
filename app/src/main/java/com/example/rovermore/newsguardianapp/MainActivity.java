4f1689710b755aedcb12ecf34a62fdc7157623ac
Roberto Moreno <robertomoreno@MacBook-Pro.local>
2017-12-17 10:03:16 +0100

    nuevo update

--- app/src/main/java/com/example/rovermore/newsguardianapp/MainActivity.java
+++ app/src/main/java/com/example/rovermore/newsguardianapp/MainActivity.java
@@ -1,15 +1,90 @@
 package com.example.rovermore.newsguardianapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Loader;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView emptyStateView;
+    private ProgressBar progressBar;
+
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private EditText userQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private void createUI(final ArrayList<Noticia> bookArrayList){
+
+
+
+        NoticiaAdapter adapter = null;
+
+        adapter = new NoticiaAdapter(this, bookArrayList);
+
+        ListView booksListView = (ListView) findViewById(R.id.list_item);
+
+        booksListView.setEmptyView(emptyStateView);
+
+        booksListView.setAdapter(adapter);
+
+
+    }
+
+
+
+    public Loader<ArrayList<Noticia>> onCreateLoader(int i, Bundle bundle){
+
+        final String userSearchText= userQuery.getText().toString();
+        Log.v("onCreateLoader","onCreate loader running loader should be created");
+
+        NoticiaLoader noticiaLoader = new NoticiaLoader(this, userSearchText);
+
+
+
+        return noticiaLoader;
+    }
+
+
+    public void onLoadFinished(Loader<ArrayList<Noticia>> loader, ArrayList<Noticia> noticia) {
+
+        Log.v("onLoadFinished","update of the UI should start and create de interface");
+
+
+
+        //Makes progerss bar disappear when the loader is loaded
+        progressBar.setVisibility(View.GONE);
+
+        createUI(noticia);
+
+
+        getLoaderManager().destroyLoader(0);
+
+
+    }
+
+
+    public void onLoaderReset(Loader<ArrayList<Noticia>> loader) {
+
+        Log.v("onLoaderReset","Create a new arraylist");
+        createUI(new ArrayList<Noticia>());
+
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/rovermore/newsguardianapp/NoticiaAdapter.java
+++ app/src/main/java/com/example/rovermore/newsguardianapp/NoticiaAdapter.java
@@ -1,7 +1,6 @@
 package com.example.rovermore.newsguardianapp;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -18,7 +17,7 @@ import java.util.ArrayList;
 
 public class NoticiaAdapter extends ArrayAdapter<Noticia> {
 
-    public NoticiaAdapter(Context context, @LayoutRes int resource, @NonNull ArrayList<Noticia> Noticia) {
+    public NoticiaAdapter(Context context, @NonNull ArrayList<Noticia> Noticia) {
         super(context, 0, Noticia);
     }
 
